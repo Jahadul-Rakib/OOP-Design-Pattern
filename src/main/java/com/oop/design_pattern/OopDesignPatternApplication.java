28f2992ee5d487bb9e1a3ca6d201ae04224bbc55
Jahadul-Rakib <rakibdiu2015@gmail.com>
2022-05-12 10:18:13 +0600

    add

---
+++ src/main/java/com/oop/design_pattern/OopDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.oop.design_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (OopDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oop/design_pattern/OopDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
