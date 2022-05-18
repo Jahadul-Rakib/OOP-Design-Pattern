00cb552e03f051940fc3ac6ac1a1b149a4d313fa
Rabbi <golam.rabbi@klovercloud.com>
2022-05-19 00:08:24 +0600

    Abstract factory pattern complete

---
+++ src/main/java/com/oop/design_pattern/creational/abstract_factory/electornics/mobile/Mobile.java
@@ -0,0 +1,6 @@
+package com.oop.design_pattern.creational.abstract_factory.electornics.mobile;
+
+
+public interface Mobile {
+    public String create();
+}
--- src/main/java/com/oop/design_pattern/creational/factory_method/mobile/Mobile.java
+++ src/main/java/com/oop/design_pattern/creational/factory_method/mobile/Mobile.java
@@ -1,5 +1,7 @@
 package com.oop.design_pattern.creational.factory_method.mobile;
 
+import com.oop.design_pattern.creational.factory_method.enums.MobileTypes;
+
 public interface Mobile {
     String createMobile();
 }
