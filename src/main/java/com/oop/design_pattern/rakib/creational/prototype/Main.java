0f9955cac174c1c056d6d4ccfcac3b06de547d5e
Jahadul-Rakib <rakibdiu2015@gmail.com>
2022-06-05 09:40:16 +0600

    add

--- src/main/java/com/oop/design_pattern/rakib/creational/prototype/Main.java
+++ src/main/java/com/oop/design_pattern/rakib/creational/prototype/Main.java
@@ -5,12 +5,12 @@ import com.oop.design_pattern.rakib.creational.prototype.classes.ODDI;
 
 public class Main {
     public static void main(String[] args) {
-        BMW bmw = new BMW ("A3","HEAVY", 12L);
+        BMW bmw = new BMW ("A3", "HEAVY", 12L);
         Car clone = bmw.clone ();
         System.out.println (clone);
 
-        ODDI oddi = new ODDI ("O3","SUPER_HEAVY", 22L);
-        Car clone2 = oddi.clone ();
+        ODDI oddi = new ODDI ("O3", "SUPER_HEAVY", 22L);
+        ODDI clone2 = (ODDI) oddi.clone ();
         System.out.println (clone2);
     }
 }
